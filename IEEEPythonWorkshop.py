8e6772b0c025a3125d319189698d1cfc17fa9863
Iordanis Kostelidis <Kostelidis@IEEE.org>
2019-05-27 23:11:05 +0300

    Create array, read input and handle the user's response (with if)

--- IEEEPythonWorkshop.py
+++ IEEEPythonWorkshop.py
@@ -6,3 +6,20 @@ print("What's your name?")
 name = input("My name is ")
 
 print("Hello", name)
+
+print("You want to calculate your passed and non-passed lessons?")
+
+positiveAnswers = ['yes', 'y', 'yep']
+negativeAnswers = ['no', 'n', 'nop']
+
+answer = input()
+
+if answer not in negativeAnswers and answer not in positiveAnswers:
+    print("I don't understand, please run my again. Bye")
+    quit()
+
+if answer in negativeAnswers:
+    print("OK, good bye...")
+    quit()
+
+print("TODO")
